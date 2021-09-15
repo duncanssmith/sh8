6fb4690fa26e5a63ce35571095a584534f8ba62e
Duncan Smith <duncanssmith@gmail.com>
2021-09-15 15:05:32 +0100

    Page styling for sort pages

--- resources/views/admin/texts/sort.blade.php
+++ resources/views/admin/texts/sort.blade.php
@@ -1,7 +1,8 @@
 <x-layout :uuid=$uuid :path="$path">
 
-    <h1>Sort {{ $category->name }} texts</h1>
-    <p>Click and drag items below to arrange in the desired order</p>
+    <h1 class="pt-8 py-4 text-gray-500 font-semibold">{{ $category->name }} texts</h1>
+    <p class="text-sm text-gray-500 font-normal">The texts listed below are currently on the {{$category->name}} page</p>
+    <p class="text-sm text-gray-500 font-normal">Click and drag to arrange in the desired order</p>
 
     <!-- will be used to show any messages -->
     @if (Session::has('message'))
@@ -11,27 +12,27 @@
     <div class="py-4 px2">
         <table class="border category" data-categoryid="{{ $category->id}}">
             <thead>
-            <tr>
+            <tr class="text-gray-500">
 
-                <th class="px-4 py-4 border border-solid text-gray-500"><i class="fa fa-sort"></i></th>
+                <th class="px-4 py-4 border border-solid"><i class="fa fa-sort" title="Drag and drop rows to sort"></i></th>
 {{--                <th class="border border-solid ">Sort-order</th>--}}
 {{--                <th class="border border-solid ">Id</th>--}}
-                <th class="border border-solid px-4 text-gray-500">Title</th>
+                <th class="border border-solid px-4 ">Title</th>
 {{--                <th class="border border-solid">Slug</th>--}}
-                <th class="border border-solid px-4 text-gray-500">Body</th>
-                <th class="border border-solid px-4 text-gray-500">Author</th>
-                <th class="border border-solid px-4 text-gray-500">Year</th>
-                <th class="border border-solid px-4 text-gray-500">Description</th>
-                <th class="border border-solid px-4 text-gray-500">Publication</th>
-                <th class="border border-solid px-4 text-gray-500">Publication date</th>
+                <th class="border border-solid px-4 ">Body</th>
+                <th class="border border-solid px-4 ">Author</th>
+                <th class="border border-solid px-4 ">Year</th>
+                <th class="border border-solid px-4 ">Description</th>
+                <th class="border border-solid px-4 ">Publication</th>
+                <th class="border border-solid px-4 ">Publication date</th>
 
             </tr>
             </thead>
             <tbody id="sortable" class="sortable ui-sortable">
             @foreach($texts as $key => $text)
-                <tr class="ui-state-default ui-sortable-handle " data-id="{{ $text->id }}">
+                <tr class="ui-state-default ui-sortable-handle text-gray-500" data-id="{{ $text->id }}">
 
-                    <td class="px-4 py-4 text-gray-500 "><i class="fa fa-random" title="sort"></i></td>
+                    <td class="px-4 py-4 "><i class="fa fa-random text-green-500" title="sort"></i></td>
 {{--                    <td class="border border-solid ">{{ $text->pivot->order }}</td>--}}
 {{--                    <td class="border border-solid ">{{ $text->id }}</td>--}}
                     <td class="border border-solid py-2 px-3" title="id:[{{$text->id}}] order:[{{$text->pivot->order}}]">{{ $text->title }}</td>
--- resources/views/admin/works/sort.blade.php
+++ resources/views/admin/works/sort.blade.php
@@ -1,7 +1,8 @@
 <x-layout :uuid=$uuid :path="$path">
 
-    <h1>Sort {{ $category->name }} works</h1>
-    <p>Click and drag items below to arrange in the desired order</p>
+    <h1 class="pt-8 py-4 text-gray-500 font-semibold">Sort {{ $category->name }} works</h1>
+    <p class="text-sm text-gray-500 font-normal">The works listed below are currently on the {{$category->name}} page</p>
+    <p class="text-sm text-gray-500 font-normal">Click and drag to arrange in the desired order</p>
 
     <!-- will be used to show any messages -->
     @if (Session::has('message'))
@@ -11,33 +12,33 @@
     <div class="py-4 px2">
         <table class="category py-4 px-2" data-categoryid="{{ $category->id}}">
             <thead>
-            <tr>
+            <tr class="text-gray-500">
 
-                <th class="px-4 py-4 border border-solid text-gray-500"><i class="fa fa-sort"></i></th>
-                <th class="border border-solid ">Sort-order</th>
-                <th class="border border-solid ">Id</th>
-                <th class="border border-solid">Thumbnail</th>
-                <th class="border border-solid">Title</th>
-                <th class="border border-solid">Slug</th>
-                <th class="border border-solid">Media</th>
-                <th class="border border-solid">Dimensions</th>
-                <th class="border border-solid">Work date</th>
+                <th class="px-4 py-4 border border-solid"><i class="fa fa-sort text-blue-500" title="Drag and drop rows to sort"></i></th>
+{{--                <th class="border border-solid ">Sort-order</th>--}}
+{{--                <th class="border border-solid ">Id</th>--}}
+                <th class="border border-solid px-4">Thumbnail</th>
+                <th class="border border-solid px-4">Title</th>
+                <th class="border border-solid px-4">Slug</th>
+                <th class="border border-solid px-4">Media</th>
+                <th class="border border-solid px-4">Dimensions</th>
+                <th class="border border-solid px-4">Work date</th>
 
             </tr>
             </thead>
             <tbody id="sortable" class="sortable ui-sortable">
             @foreach($works as $key => $work)
-                <tr class="ui-state-default ui-sortable-handle" data-id="{{ $work->id }}">
+                <tr class="ui-state-default ui-sortable-handle text-gray-500" data-id="{{ $work->id }}">
 
-                    <td class="px-4 py-4 text-gray-500 "><i class="fa fa-random"></i></td>
-                    <td class="border border-solid ">{{ $work->pivot->order }}</td>
-                    <td class="border border-solid ">{{ $work->id }}</td>
-                    <td class="border border-solid"><img src="{{ asset( $work->thumbnail )}}" width="15%"/></td>
-                    <td>{{ $work->title }}</td>
-                    <td>{{ $work->slug }}</td>
-                    <td>{{ $work->media }}</td>
-                    <td>{{ $work->dimensions }}</td>
-                    <td>{{ $work->work_date }}</td>
+                    <td class="px-4 py-4 text-blue-500 "><i class="fa fa-random" title="s:[{{$work->pivot->order}}]"></i></td>
+{{--                    <td class="border border-solid ">{{ $work->pivot->order }}</td>--}}
+{{--                    <td class="border border-solid ">{{ $work->id }}</td>--}}
+                    <td class="border border-solid px-4 py-2"><img src="{{ asset( $work->thumbnail )}}" width="15%" title="{{ $work->id }}"/></td>
+                    <td class="border border-solid px-4">{{ $work->title }}</td>
+                    <td class="border border-solid px-4">{{ $work->slug }}</td>
+                    <td class="border border-solid px-4">{{ $work->media }}</td>
+                    <td class="border border-solid px-4">{{ $work->dimensions }}</td>
+                    <td class="border border-solid px-4">{{ $work->work_date }}</td>
 
                 </tr>
             @endforeach
