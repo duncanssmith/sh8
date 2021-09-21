dd7192c06a98892ca35fca26bfabdf1c1bb6be2b
Duncan Smith <duncanssmith@gmail.com>
2021-09-21 16:35:16 +0100

    Make edit views consistent

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,15 +1,16 @@
 <x-layout>
-    <x-setting heading="Edit page">
-{{--    <h1 class="text-xl text-gray-700 font-bold py-6">Edit page</h1>--}}
-        <form method="POST" action="/admin/category/{{ $category->id }}">
-            @csrf
-            @method('PATCH')
-            <x-form.input name="name" :value="old('name', $category->name)" required />
-            <x-form.input name="slug" :value="old('slug', $category->slug)" required />
-            <x-form.checkbox name="display" :value="old('display', $category->display)" id="{{ $category->display }}" checked="{{ $category->display }}" />
-            <x-form.submit>Update</x-form.submit>
-            <x-form.cancel>Cancel</x-form.cancel>
-        </form>
+    <section class="px-6 py-8">
+        <x-setting heading="Edit category">
+            <form method="POST" action="/admin/category/{{ $category->id }}">
+                @csrf
+                @method('PATCH')
+                <x-form.input name="name" :value="old('name', $category->name)" required />
+                <x-form.input name="slug" :value="old('slug', $category->slug)" required />
+                <x-form.checkbox name="display" :value="old('display', $category->display)" id="{{ $category->display }}" checked="{{ $category->display }}" />
+                <x-form.submit>Update</x-form.submit>
+                <x-form.cancel>Cancel</x-form.cancel>
+            </form>
 
-    </x-setting>
+        </x-setting>
+    </section>
 </x-layout>
