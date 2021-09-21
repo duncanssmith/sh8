1b7373dc063bd50bbb37ace6531d0f6acdeb449f
Duncan Smith <duncanssmith@gmail.com>
2021-09-21 16:45:41 +0100

    Make index pages consistent

--- resources/views/admin/works/index.blade.php
+++ resources/views/admin/works/index.blade.php
@@ -1,64 +1,65 @@
 <x-layout>
+    <section class="px-6 py-8">
+        <x-setting heading="Manage works">
+            <div class="flex flex-col">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
 
-    <x-setting heading="Manage works" count="{{ count($works) }}">
-        <div class="flex flex-col">
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-
-                        <table class="min-w-full divide-y divide-gray-200">
-                            <thead class="bg-white divide-y divide-gray-200">
-                                <tr>
-                                    <th class="px-6 py-4 whitespace-nowrap text-gray-700"> Image </th>
-                                    <th class="px-6 py-4 whitespace-nowrap text-gray-700"> Title </th>
-                                    <th class="px-6 py-4 whitespace-nowrap">&nbsp;</th>
-                                    <th class="px-6 py-4 whitespace-nowrap">&nbsp;</th>
-                                    <th class="px-6 py-4 whitespace-nowrap">&nbsp;</th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($works as $work)
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        @if ($work->thumbnail)
-                                            <a href="/works/{{ $work->slug }}">
-                                                <img src="{{ asset($work->thumbnail) }}" width="10%"/>&nbsp;
-                                            </a>
-                                        @endif
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="flex items-center">
-                                            <div class="text-sm text-gray-600">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-white divide-y divide-gray-200">
+                                    <tr>
+                                        <th class="px-6 py-4 whitespace-nowrap text-gray-700"> Image </th>
+                                        <th class="px-6 py-4 whitespace-nowrap text-gray-700"> Title </th>
+                                        <th class="px-6 py-4 whitespace-nowrap">&nbsp;</th>
+                                        <th class="px-6 py-4 whitespace-nowrap">&nbsp;</th>
+                                        <th class="px-6 py-4 whitespace-nowrap">&nbsp;</th>
+                                    </tr>
+                                </thead>
+                                <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach ($works as $work)
+                                    <tr>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            @if ($work->thumbnail)
                                                 <a href="/works/{{ $work->slug }}">
-                                                    {{ $work->title }}
+                                                    <img src="{{ asset($work->thumbnail) }}" width="10%"/>&nbsp;
                                                 </a>
+                                            @endif
+                                        </td>
+                                        <td class="px-6 py-4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <div class="text-sm text-gray-600">
+                                                    <a href="/works/{{ $work->slug }}">
+                                                        {{ $work->title }}
+                                                    </a>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
+                                        </td>
 
-                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="/admin/work/{{ $work->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
-                                    </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="/admin/work/{{ $work->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
+                                        </td>
 
-                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="/admin/category/assign_work/{{ $work->id }}" class="text-blue-500 hover:text-blue-600">Assign</a>
-                                    </td>
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <a href="/admin/category/assign_work/{{ $work->id }}" class="text-blue-500 hover:text-blue-600">Assign</a>
+                                        </td>
 
-                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <form method="POST" action="/admin/work/{{ $work->id }}">
-                                            @csrf
-                                            @method('DELETE')
+                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                            <form method="POST" action="/admin/work/{{ $work->id }}">
+                                                @csrf
+                                                @method('DELETE')
 
-                                            <button class="text-xs text-gray-400">Delete</button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
+                                                <button class="text-xs text-gray-400">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </x-setting>
+        </x-setting>
+    </section>
 </x-layout>
